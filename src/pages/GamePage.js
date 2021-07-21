4ba7d6ab363f3c98d314a5c693e3ded3afcbbf2a
2ManyProjects <shaivkamat@gmail.com>
2021-07-21 00:25:42 -0700

    log

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -1,4 +1,5 @@
 import React, { PureComponent } from "react";
+import qs from "qs";
 import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import {
   Button,
@@ -15,8 +16,12 @@ const window = {
   width: 800,
   height: 600,
 };
+const cID = "81e488290580a291f526433741c171bf66d173f367ef06be45c65de482a3e1c0";
+const CS = "5e6309f766985e8edbb41d6b451e29a0a6c2e8dbdddafe9232c58740985adbc1";
 export default class Game extends PureComponent {
   render() {
+    let id = qs.parse(window.location.search);
+    console.log(qs.parse(window.location.search));
     return (
       <Container
         style={{
