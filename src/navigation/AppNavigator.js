14c9539f79c0a4e79f2a977c396792228f53d58c
2ManyProjects <shaivkamat@gmail.com>
2021-07-21 00:55:35 -0700

    log

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -2,51 +2,9 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import Home from "../pages/HomePage";
 import GamePage from "../pages/GamePage";
-import { makeStyles } from "@material-ui/core/styles";
-import { Navbar } from "shards-react";
 import { isMobile } from "react-device-detect";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-}));
-
-function useWindowSize() {
-  // Initialize state with undefined width/height so server and client renders match
-  const [windowSize, setWindowSize] = useState({
-    width: undefined,
-    height: undefined,
-  });
-
-  useEffect(() => {
-    // Handler to call on window resize
-    function handleResize() {
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
-    }
-
-    // Add event listener
-    window.addEventListener("resize", handleResize);
-    handleResize();
-
-    // Remove event listener on cleanup
-    return () => window.removeEventListener("resize", handleResize);
-  }, []); // Empty array ensures that effect is only run on mount
-
-  return windowSize;
-}
-
 function Navigator(props) {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-
-  const size = useWindowSize();
-  const classes = useStyles();
   return (
     <Router>
       <div
--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -20,8 +20,8 @@ const cID = "81e488290580a291f526433741c171bf66d173f367ef06be45c65de482a3e1c0";
 const CS = "5e6309f766985e8edbb41d6b451e29a0a6c2e8dbdddafe9232c58740985adbc1";
 export default class Game extends PureComponent {
   render() {
-    let id = qs.parse(window.location.href);
-    console.log(qs.parse(window.location.href));
+    let id = qs.parse(this.props.location.search);
+    console.log(qs.parse(this.props.location.search));
     return (
       <Container
         style={{
