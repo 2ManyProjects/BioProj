72dfa6b941f224aa98a09d37732d532d543cb5f1
2ManyProjects <shaivkamat@gmail.com>
2021-07-20 18:56:51 -0700

    feat: remade template repo

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import Home from "../pages/HomePage";
 import GamePage from "../pages/GamePage";
-import Citation from "../pages/CitationPage";
 import { makeStyles } from "@material-ui/core/styles";
 import { Navbar } from "shards-react";
 import { isMobile } from "react-device-detect";
@@ -64,87 +63,9 @@ function Navigator(props) {
       />
       <Route
         exact
-        path="/game"
+        path="/test"
         render={(props) => <GamePage {...props} isMobile={isMobile} />}
       />
-      <Route
-        exact
-        path="/citation"
-        render={(props) => <Citation {...props} />}
-      />
-
-      <Navbar
-        style={{
-          marginBottom: 75,
-          position: "absolute",
-          top: "1%",
-          width: "100%",
-          display: "flex",
-          "flex-direction": "row",
-          "justify-content": "space-around",
-          background: "#96D559",
-          // left: "50%",
-        }}
-        type="dark"
-        theme=""
-        expand="md"
-      >
-        <div>
-          <Link
-            to="/"
-            style={{
-              justifyContent: "flex-start",
-            }}
-          >
-            {" "}
-            <text
-              style={{
-                color: "white",
-                fontSize: 20,
-                fontWeight: "bold",
-              }}
-            >
-              Home
-            </text>
-          </Link>
-        </div>
-        <div>
-          <Link
-            to="/game"
-            style={{
-              justifyContent: "flex-start",
-            }}
-          >
-            <text
-              style={{
-                color: "white",
-                fontSize: 20,
-                fontWeight: "bold",
-              }}
-            >
-              GamePage
-            </text>
-          </Link>
-        </div>
-        <div>
-          <Link
-            to="/citation"
-            style={{
-              justifyContent: "flex-start",
-            }}
-          >
-            <text
-              style={{
-                color: "white",
-                fontSize: 20,
-                fontWeight: "bold",
-              }}
-            >
-              Citations
-            </text>
-          </Link>
-        </div>
-      </Navbar>
     </Router>
   );
 }
--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -10,10 +10,6 @@ import {
   CardTitle,
   CardSubtitle,
 } from "shards-react";
-import legend from "../res/Legend.png";
-import { GameEngine } from "react-game-engine";
-import { Box, Macrophage, Site, Menu, Prog } from "../components/renderers";
-import { addEnbrel, UpdateEntities, menuCheck } from "../components/systems";
 
 const window = {
   width: 800,
@@ -32,53 +28,7 @@ export default class Game extends PureComponent {
           paddingTop: "150px",
         }}
         className="dr-example-container"
-      >
-        {/* *****************************************ENGINE **************************************** */}
-        <GameEngine
-          style={{ ...window, backgroundColor: "#710C04" }}
-          systems={[addEnbrel, UpdateEntities, menuCheck]}
-          entities={{
-            gameTable: { m: 1, t: 0, e: 0, window: window },
-            menu: {
-              stage: 0,
-              title: [
-                "Enbrel Simulation",
-                "R.I.P your aneurysm ruptured",
-                "Congrats! You lived a minute!",
-              ],
-              subtitle: ["Start Game", "Another Go?", "Another Go?"],
-              renderer: <Menu />,
-            },
-            prog: {
-              stage: 0,
-              count: 0,
-              maxCount: 6000,
-              renderer: <Prog />,
-            },
-          }}
-        ></GameEngine>
-        <img src={legend} />
-        <p
-          style={{
-            width: "800px",
-            color: "black",
-            fontSize: 15,
-            fontWeight: "bold",
-          }}
-        >
-          In this little Enbrel Simulation, you are attempting to surpress an
-          overactive immune response. Enbrel does this by binding to and
-          blocking the macrophages TNF protiens which alert them to immune
-          situations. The amount and severity of the macrophages response is
-          proportional to the amount of TNF that has bound to the inflammation
-          sites. To keep them away you need to deploy Enbrel both to prevent the
-          macrophages from targeting the inflammation site as well as to keep
-          their response as slow/low as possible. The goal is to survive till
-          you hit 100%, If the macrophages reach the inflammation site, the site
-          will inflamme faster and faster until . . .
-        </p>
-        {/* *****************************************ENGINE **************************************** */}
-      </Container>
+      ></Container>
     );
   }
 }
